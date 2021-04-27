10faeb7e9371217530b55d44a937b5d29d2d4ac5
Carlos Tiago <carlostiagodesousa@gmail.com>
2021-04-26 23:50:49 -0300

    aplicando token em todas as requisições http

--- src/app/core/AbstractService.ts
+++ src/app/core/AbstractService.ts
@@ -5,13 +5,19 @@ export abstract class AbstractService {
   constructor() { }
 
   httpOptions(params: HttpParams = new HttpParams()): any {
-    let headers = new HttpHeaders();
-    headers = headers.append('Authorization', 'Basic YWRtaW5AZW1haWwuY29tOmFkbWlu');
-    headers = headers.append('Content-Type', 'application/json');
-
     return {
-      headers,
+      headers: this.getHttpHeaders(),
       params
     };
+  }
+
+  private getHttpHeaders(): HttpHeaders {
+    let headers = new HttpHeaders();
+    const token = localStorage.getItem('token');
+
+    headers = headers.append('Authorization', `Bearer ${token}`);
+    headers = headers.append('Content-Type', 'application/json');
+
+    return headers;
   }
 }
