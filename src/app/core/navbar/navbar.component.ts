cf3839318c767a9f4b1d0ff0d3f41109017b93f8
Carlos Tiago <carlostiagodesousa@gmail.com>
2021-04-27 00:07:54 -0300

    correcao do bug ao atualizar status e incluindo nome de usuario logado

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../seguranca/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -10,12 +11,19 @@ export class NavbarComponent implements OnInit {
 
   exibirMenu: boolean;
 
-  constructor( private router: Router ) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
   exibirToggle(): boolean {
     return this.router.url !== '/login';
+  }
+
+  usuarioLogado(): string {
+    return this.authService.jwtPayload?.nome;
   }
 }
